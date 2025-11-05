8bf428845f7a961be848c5e899ac87b63a64089c
SALAHEDDINE <esaissielismailisalaheddine@gmail.com>
2025-11-05 23:04:18 +0100

    Load and display raw data in Streamlit app
    Added functionality to load and display raw data from a CSV file.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
-
+import numpy as np
 st.title('🤖 Machine Learning App')
 
 st.info('This is app builds a machine lerning model!')
+
+with st.expander('Data'):
+  st.write('**Raw data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  df
